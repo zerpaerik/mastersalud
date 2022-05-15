b9526eb7bf8fb6096a65498d9beab31290cabea0
Erik Zerpa <ezerpa@sipecom.com>
2022-05-15 11:39:51 -0400

    aa

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1354,33 +1354,7 @@
             </ul>
           </li>
 
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-money-check-alt"></i>
-              <p>
-                Métodos Anticonceptivos
-                <i class="fas fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-
-
-            <li class="nav-item">
-                <a href="{{route('metodos.index')}}" class="nav-link">
-                  <i class="fas fa-hand-holding-usd nav-icon"></i>
-                  <p>Lista de Métodos</p>
-                </a>
-              </li>
-
-              <li class="nav-item">
-                <a href="{{route('llamar.index')}}" class="nav-link">
-                  <i class="fas fa-hand-holding-usd nav-icon"></i>
-                  <p>Pacientes por Llamar</p>
-                </a>
-              </li>
-              
-            </ul>
-          </li>
+         
 
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
@@ -1413,6 +1387,39 @@
               
             </ul>
           </li>
+
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-money-check-alt"></i>
+              <p>
+                Existencias
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+
+
+            <li class="nav-item">
+                <a href="{{route('productos.index')}}" class="nav-link">
+                  <i class="fas fa-hand-holding-usd nav-icon"></i>
+                  <p>Productos</p>
+                </a>
+              </li>
+
+
+              <li class="nav-item">
+                <a href="{{route('ventas.index')}}" class="nav-link">
+                  <i class="fas fa-cart-arrow-down nav-icon"></i>
+                  <p>Ventas</p>
+                </a>
+              </li>
+
+            
+            
+              
+            </ul>
+          </li>
+
 
         
 
@@ -1449,11 +1456,6 @@
 
        
          
-
-        
-
-        
-
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-tools"></i>
