b1475c0c07832b278f423e7c1bf0f1aa88041559
Erik Zerpa <zerpaerik@gmail.com>
2025-01-10 13:50:13 -0400

    aaa

--- resources/views/consultas/index.blade.php
+++ resources/views/consultas/index.blade.php
@@ -144,6 +144,15 @@
                     @if($an->tipo_consulta == 6)
                           @if($an->triaje == 0)
 
+                          @if(Auth::user()->rol == 1)
+                             <a class="btn btn-secondary btn-sm" href="consultas-triaje-{{$an->id}}">
+                                  <i class="fas fa-pencil-alt">
+                                  </i>
+                                  Triaje
+                              </a>  
+
+                              @endif
+
                              @if(Auth::user()->rol == 7)
                              <a class="btn btn-secondary btn-sm" href="consultas-triaje-{{$an->id}}">
                                   <i class="fas fa-pencil-alt">
