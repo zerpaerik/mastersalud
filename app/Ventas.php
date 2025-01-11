4b9faa39295f8853a7a21b33aacdf1a0e9744bb9
Erik Zerpa <zerpaerik@gmail.com>
2025-01-10 20:31:46 -0400

    aaa

--- app/Ventas.php
+++ app/Ventas.php
@@ -47,13 +47,64 @@ class Ventas extends Model
 
 public function detallePago($id)
 {
+ 
 
-    $array='';
+  $array='';
+  
+  $ef = DB::table('creditos as a')
+  ->select('a.id_venta','a.tipopago',DB::raw('SUM(a.monto) as efectivo'))
+  ->where('a.tipopago', '=', 'EF')
+  ->where('a.id_venta', '=', $id)
+  ->get(); 
+
+
+  $tj = DB::table('creditos as a')
+  ->select('a.id_venta','a.tipopago',DB::raw('SUM(a.monto) as tj'))
+  ->where('a.tipopago', '=', 'TJ')
+  ->where('a.id_venta', '=', $id)
+  ->get(); 
+
+  $dp = DB::table('creditos as a')
+  ->select('a.id_venta','a.tipopago',DB::raw('SUM(a.monto) as dp'))
+  ->where('a.tipopago', '=', 'DP')
+  ->where('a.id_venta', '=', $id)
+  ->get(); 
+
+  $yp = DB::table('creditos as a')
+  ->select('a.id_venta','a.tipopago',DB::raw('SUM(a.monto) as yp'))
+  ->where('a.tipopago', '=', 'YP')
+  ->where('a.id_venta', '=', $id)
+  ->get(); 
+
+  $descripcion='';
+
+  foreach ($ef as $key => $value) {
+    $descripcion.= $value->efectivo.'-'.$value->tipopago.' ';
+ }
+
+ foreach ($tj as $key => $value) {
+  $descripcion.= $value->tj.'-'.$value->tipopago.' ';
+}
+
+foreach ($dp as $key => $value) {
+  $descripcion.= $value->dp.'-'.$value->tipopago.' ';
+}
+
+foreach ($yp as $key => $value) {
+  $descripcion.= $value->yp.'-'.$value->tipopago.' ';
+}
+
+return substr($descripcion, 0, -1);
+
+
+
+  /*  $array='';
     $data = \DB::table('ventas_detalle')
     ->select('*')
                // ->where('estatus','=','1')
     ->where('id_venta', $id)
     ->get();
+
     $descripcion='';
     
     
@@ -61,7 +112,7 @@ public function detallePago($id)
       $descripcion.= $value->monto.'-'.$value->tipop.','.$value->monto1.'-'.$value->tipop1.' ';
    }
 
-return substr($descripcion, 0, -1);
+return substr($descripcion, 0, -1);*/
           //  return $id;
 }
 
